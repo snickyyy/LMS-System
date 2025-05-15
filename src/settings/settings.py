a359f00851586352000df2f33c07309f4193de95
snickyyy <leon12345671234@gmail.com>
2025-05-15 21:05:08 +0200

    add: PostgresSettings in settings.py

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -4,8 +4,20 @@ from functools import lru_cache
 
 from pydantic_settings import BaseSettings
 
+class PostgresSettings(BaseSettings):
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+    class Config:
+        env_file = os.path.join(pathlib.Path(__file__).resolve().parent.parent.parent, ".env")
+
 class Settings(BaseSettings):
     BASE_PATH: str = str(pathlib.Path(__file__).resolve().parent.parent.parent)
+
+    POSTGRES: PostgresSettings = PostgresSettings()
 
     class Config:
         env_file = os.path.join(pathlib.Path(__file__).resolve().parent.parent.parent, ".env")
