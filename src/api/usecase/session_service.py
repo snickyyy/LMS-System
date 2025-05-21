f6af7e7e567cd3651a604582a207068fdb6679ee
snickyyy <leon12345671234@gmail.com>
2025-05-21 21:36:17 +0200

    add: handling error in session_service.py

--- src/api/usecase/session_service.py
+++ src/api/usecase/session_service.py
@@ -1,6 +1,7 @@
 from functools import lru_cache
 from uuid import uuid4
 
+from api.errors.usecase import BadRequestError
 from api.repositories.base_redis_repository import (
     BaseRedisRepository,
     get_redis_repository,
@@ -14,13 +15,21 @@ class SessionService:
         self.redis_repository = redis_repository
 
     async def create_session(self, session: BaseSession) -> uuid4:
-        to_json = session.model_dump_json()
+        try:
+            to_json = session.model_dump_json()
+        except Exception as e:
+            raise BadRequestError("Invalid session") from e
         await self.redis_repository.set(session.prefix, str(session.session_id), to_json, session.exp)
         return session.session_id
 
     async def get_session(self, prefix: str, session_id: str, decrypt_payload=False) -> BaseSession:
         session = await self.redis_repository.get(prefix, session_id)
-        session = BaseSession.model_validate_json(session)
+        if not session:
+            raise BadRequestError("Invalid session")
+        try:
+            session = BaseSession.model_validate_json(session)
+        except Exception as e:
+            raise BadRequestError("Invalid session") from e
         if decrypt_payload:
             session.payload = decrypt(session.payload)
         return session
