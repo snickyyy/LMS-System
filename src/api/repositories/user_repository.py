68210c7b0dfeef88fdcb6303d9665a36f5956bd3
snickyyy <leon12345671234@gmail.com>
2025-05-23 14:35:55 +0200

    add: in get_by_email UserRepository

--- src/api/repositories/user_repository.py
+++ src/api/repositories/user_repository.py
@@ -1,5 +1,8 @@
 from functools import lru_cache
 
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from api.models import User
 from api.repositories.base_postgres_repository import BasePostgresRepository
 
@@ -7,6 +10,11 @@ from api.repositories.base_postgres_repository import BasePostgresRepository
 class UserRepository(BasePostgresRepository[User]):
     def __init__(self):
         super().__init__(User)
+
+    async def get_by_email(self, session: AsyncSession, email: str) -> User | None:
+        stmt = select(self._model).where(self._model.email == email)
+        result = await session.execute(stmt)
+        return  result.scalar_one_or_none()
 
 @lru_cache
 def get_user_repository() -> UserRepository:
